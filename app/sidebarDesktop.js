b0ae00e3582844380bf1e992304e2d5f73eb19fb
shivaniDev <shivanisikarwar2003@gmail.com>
2024-06-12 11:14:55 +0530

    abc

---
+++ app/sidebarDesktop.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const sidebarDesktop = () => {
+  return (
+//    <aside className='w-[270px] mx-w-xs h-screen left-0 top-0 z-40 border-r'>
+// <div className='h-full px-3 py-4'>
+//     <h3 className='mx-3 text-lg  font-femobold text-foreground'>twiter</h3>
+
+// </div>
+//    </aside>
+<div>
+  <h3>sdfghj</h3>
+</div>
+  )
+}
+
+export default sidebarDesktop
